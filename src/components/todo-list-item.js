1b63b18279baeb6fe5e5c3a854001870d356f35d
Dmitry Sherbakov <dvsherbakov@gmail.com>
2019-04-08 22:37:56 +0500

    222

--- src/components/todo-list-item.js
+++ src/components/todo-list-item.js
@@ -1,17 +1,38 @@
 import React from 'react';
 import './todo-list-item.css';
 
-class TodoListItem extends React.Component { 
+class TodoListItem extends React.Component {
+    constructor() {
+        super();
+
+        this.onLabelClick = () => {
+            this.setState(
+                {
+                    done: true
+                }
+            );
+        }
+
+        this.state = {
+            done: false
+        }
+    }
 
     render() {
-        const { name, important=false } = this.props;
+        const { name, important = false } = this.props;
+        const { done } = this.state;
+        let classNames = 'normalSpan';
+        if (done) classNames += ' doneSpan';
         const style = {
-            color : important ? 'steelblue' : 'black',
-            fontWeight : important ? 'bold' : 'normal'
+            color: important ? 'steelblue' : 'black',
+            fontWeight: important ? 'bold' : 'normal'
         }
         return (
             <div className="itemContainer" style={style}>
-                <span>
+                <span
+                    style={style}
+                    className={classNames}
+                    onClick={this.onLabelClick} >
                     {name}
                 </span>
                 <button className="itemButton redButton" >
